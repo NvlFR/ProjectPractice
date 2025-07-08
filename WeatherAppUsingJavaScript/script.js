80a3377660484dd81554391c453b1450967cc04f
NvlFR <novalfr802@gmail.com>
2025-07-08 12:58:57 +0700

    Update script.js

--- WeatherAppUsingJavaScript/script.js
+++ WeatherAppUsingJavaScript/script.js
@@ -4,7 +4,7 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&";
 
 const searchBox = document.querySelector(".search input"); // Perbaikan typo 'seacrh' jadi 'search'
 const searchBtn = document.querySelector(".search button"); // Perbaikan typo 'seacrh' jadi 'search'
-
+const weatherIcon = document.querySelector(".weather-icon");
 async function checkWeather(city) {
   try {
     const response = await fetch(apiUrl + `q=${city}&appid=${apiKey}`);
@@ -25,7 +25,19 @@ async function checkWeather(city) {
     document.querySelector(".temp").innerHTML =
       Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h"; // Ditambah spasi untuk estetika
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "images/mist.png";
+    }
   } catch (error) {
     console.error("Ada masalah saat mengambil data cuaca:", error);
   }
